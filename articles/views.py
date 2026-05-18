addaca7e776f32f8ea30d07364b2dd384207bcd2
smahene <smaheninou@hotmail.com>
2026-05-18 12:32:28 +0200

    initial commit - django production ready

---
+++ articles/views.py
@@ -0,0 +1,7 @@
+from rest_framework import generics
+from .models import Article
+from .serializers import ArticleSerializer
+
+class ArticleListView(generics.ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
---
+++ config/settings/development.py
@@ -0,0 +1,13 @@
+﻿from .base import *
+
+SECRET_KEY = 'django-insecure-%@)0f@4x!j$4m3njii8-)aqkgbgm&87))x=dzp*z@ryczwbvi8'
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
