23d1d5430442654033430804f287270d8c042927
smahene <smaheninou@hotmail.com>
2026-05-18 13:54:28 +0200

    add health and error routes

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,15 @@
 from rest_framework import generics
 from .models import Article
 from .serializers import ArticleSerializer
+from django.http import JsonResponse
 
 class ArticleListView(generics.ListAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+
+def health_check(request):
+    return JsonResponse({"status": "ok", "message": "API is healthy"})
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+    return JsonResponse({"this": "will never be returned"})
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from articles.views import health_check, trigger_error
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('articles.urls')),
+    path('health/', health_check),
+    path('error/', trigger_error),
 ]
